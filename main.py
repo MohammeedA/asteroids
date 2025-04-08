4b5ebab68bfb5fa3e53f6ae6e79e30463075aa18
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-08 18:16:35 +0200

    add groups drawable and updateable to Player2

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
@@ -15,8 +17,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for player in drawable:
+            player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
