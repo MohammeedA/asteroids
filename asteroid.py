6ba189e12af3332d28e8dea7d1c13fc8cfee3353
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-08 20:07:16 +0200

    add asteroid.py and asteroidfield.py

---
+++ asteroid.py
@@ -0,0 +1,15 @@
+from circleshape import CircleShape
+from pygame import draw
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        width = 2
+        draw.circle(screen, "white", self.position, self.radius, width)
+    
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
